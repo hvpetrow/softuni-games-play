ad90a84a3e1a093daf837205175c42410fa22c01
= <hvpetrow@gmail.com>
2022-08-14 18:28:43 +0200

    Added Catalog,Details,Edit components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { Routes,Route } from 'react-router-dom';
 import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
 import { Create } from './components/Create/Create';
+import { Edit } from './components/Edit/Edit';
+import { Details } from './components/Details/Details';
+import { Catalog } from './components/Catalog/Catalog';
 
 
 function App() {
@@ -12,21 +15,19 @@ function App() {
     return (
         <div id="box">
             <Header />
-
             <main id="main-content">
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/register' element={<Register />} />
                     <Route path='/create' element={<Create />} />
-
-
+                    <Route path='/edit' element={<Edit />} />
+                    <Route path='/details' element={<Details />} />
+                    <Route path='/catalog' element={<Catalog />} />
                 </Routes>
-                
             </main>
-
         </div>
-
+        
     );
 }
 
