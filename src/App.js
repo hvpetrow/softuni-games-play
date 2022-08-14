862c2986126d8e41df3c125700a9f80d1760e515
= <hvpetrow@gmail.com>
2022-08-14 18:19:53 +0200

    Added Create Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Home } from './components/Home/Home';
 import { Routes,Route } from 'react-router-dom';
 import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
+import { Create } from './components/Create/Create';
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
                     <Route path='/' element={<Home />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/register' element={<Register />} />
+                    <Route path='/create' element={<Create />} />
+
 
                 </Routes>
                 
